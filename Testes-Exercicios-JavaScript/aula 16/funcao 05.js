824b7af934105cf2ee4b6090f71812bc540aad34
jordep <joaoppinheiro2005@gmail.com>
2024-06-03 23:52:47 -0300

    alteração em funcao05

--- Testes-Exercicios-JavaScript/aula 16/funcao 05.js
+++ Testes-Exercicios-JavaScript/aula 16/funcao 05.js
@@ -4,7 +4,7 @@ function fatorial(n) {
     if (n == 1) {
         return 1
     } else {
-        return n * fatorial(n-1)
+        return n * fatorial(n-1) // (fatorial) que chama (fatorial)
     }
 }
 
